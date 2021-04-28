680f2b19d0508d75ab7aa31f051d711856dbfe8e
Mateusz Olsztyński <matiol1@hotmail.com>
2021-04-28 23:14:59 +0200

    fix: ignore annoying warnings

--- App.tsx
+++ App.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import Providers from './navigation';
 import firebase from 'firebase/app';
 import apiKeys from './config/keys';
+import { LogBox } from 'react-native';
+
+LogBox.ignoreLogs(['Setting a timer']);
 
 const App = () => {
   if (!firebase.apps.length) {
